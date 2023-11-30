de66a5e7380e6e733cf997eb45c052ce2a2d165e
ATLASLORD <104906253+ATLASLORD@users.noreply.github.com>
2023-11-30 19:55:36 +0100

    hide post when double clic

--- script2.js
+++ script2.js
@@ -1,28 +1,39 @@
 const allBtns = document.querySelectorAll('.post-title button');
 const allPosts = document.querySelectorAll('.post-content');
 
-allBtns.forEach(function(btn, index){
-    btn.addEventListener('click', function(){
-        allPosts.forEach(function(post){
-            post.classList.remove('showPost');
-            post.classList.remove('animatePost');
-            let models = post.querySelectorAll('.model');
+allBtns.forEach(function(btn, index) {
+    btn.addEventListener('click', function() {
+        // Check if the clicked button is already active
+        if (this.classList.contains('activeBtn')) {
+            // Hide the post and deactivate the button
+            allPosts[index].classList.remove('showPost', 'animatePost');
+            this.classList.remove('activeBtn');
+            let models = allPosts[index].querySelectorAll('.model');
             models.forEach(function(model) {
                 model.classList.remove('animateModel');
             });
-        });
-        allBtns.forEach(function(btn){
-            btn.classList.remove('activeBtn');
-        });
-        this.classList.add('activeBtn');
-        allPosts[index].classList.add('showPost');
-
-        setTimeout(() => {
-            allPosts[index].classList.add('animatePost');
-            let models = allPosts[index].querySelectorAll('.model');
-            models.forEach(function(model) {
-                model.classList.add('animateModel');
+        } else {
+            // Deactivate all buttons and hide all posts
+            allBtns.forEach(function(btn) {
+                btn.classList.remove('activeBtn');
             });
-        }, 1); // Adjust this delay as needed
+            allPosts.forEach(function(post) {
+                post.classList.remove('showPost', 'animatePost');
+                let models = post.querySelectorAll('.model');
+                models.forEach(function(model) {
+                    model.classList.remove('animateModel');
+                });
+            });
+            // Activate the clicked button and show the post
+            this.classList.add('activeBtn');
+            allPosts[index].classList.add('showPost');
+            setTimeout(() => {
+                allPosts[index].classList.add('animatePost');
+                let models = allPosts[index].querySelectorAll('.model');
+                models.forEach(function(model) {
+                    model.classList.add('animateModel');
+                });
+            }, 1); // Adjust this delay as needed
+        }
     });
 });
