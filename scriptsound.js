106be217c205d00f1828996484b8db37f92eaee5
ATLASLORD <104906253+ATLASLORD@users.noreply.github.com>
2024-05-28 18:47:20 +0200

    Update scriptsound.js

--- scriptsound.js
+++ scriptsound.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.hover-sound-button');
-    const hoverSound = document.getElementById('hover-sound');
     const clickSound = document.getElementById('click-sound');
 
     buttons.forEach(button => {
         button.addEventListener('mouseenter', () => {
-            hoverSound.currentTime = 0; // Rewind to the start
+            const hoverSound = new Audio('hover-sound.mp3');
             hoverSound.play();
         });
 
