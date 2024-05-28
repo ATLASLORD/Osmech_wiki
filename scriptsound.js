ff01c80d18dc6f3ef9011d1b50d55df464cd4af9
ATLASLORD <104906253+ATLASLORD@users.noreply.github.com>
2024-05-28 19:19:02 +0200

    Update scriptsound.js

--- scriptsound.js
+++ scriptsound.js
@@ -2,11 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.click-sound-button');
     const clickSound = document.getElementById('click-sound');
 
+    if (!clickSound) {
+        console.error('Click sound element not found');
+        return;
+    }
+
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             try {
                 clickSound.currentTime = 0; // Rewind to the start
-                clickSound.play();
+                clickSound.play().then(() => {
+                    console.log('Click sound played');
+                }).catch(error => {
+                    console.error('Error playing click sound:', error);
+                });
             } catch (e) {
                 console.error('Error playing click sound:', e);
             }
